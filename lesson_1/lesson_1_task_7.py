e65611b838e57eb198891995c3ac419b87a950f3
evarakina1702 <evarakina1702@gmail.com>
2024-06-07 12:32:40 +0400

    homework1

--- lesson_1/lesson_1_task_7.py
+++ lesson_1/lesson_1_task_7.py
@@ -1,29 +1,10 @@
-def print_letter1(letter):
+def print_letter(letter):
     print(letter, end='')
 
 
-def print_letter2(letter):
-    print(letter, end='')
-
-
-def print_letter3(letter):
-    print(letter, end='')
-
-
-def print_letter4(letter):
-    print(letter, end='')
-
-def print_letter5(letter):
-    print(letter, end='')
-
-
-def print_letter6(letter):
-    print(letter, end='')
-
-
-print_letter1("S")
-print_letter2("k")
-print_letter3("y")
-print_letter4("P")
-print_letter5("r")
-print_letter6("o")
+print_letter("S")
+print_letter("k")
+print_letter("y")
+print_letter("P")
+print_letter("r")
+print_letter("o")
