720a082e168b40f1cd9a35473450f3a9e8be2f66
evarakina1702 <157830517+evarakina1702@users.noreply.github.com>
2024-07-26 22:45:22 +0400

    Update test_lesson3.py

--- lesson_6.1/test_lesson3.py
+++ lesson_6.1/test_lesson3.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
@@ -18,31 +17,25 @@ driver.maximize_window()
 driver.find_element(By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
 driver.find_element(By.CSS_SELECTOR, "#password").send_keys("secret_sauce")
 driver.find_element(By.CSS_SELECTOR, "#login-button").click()
-sleep(2)
 
 # Добавить в корзину 3 товара
 driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack").click()
 driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
 driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie").click()
-sleep(2)
 
 # Перейти в корзину
 driver.find_element(By.CSS_SELECTOR, 'a[data-test="shopping-cart-link"]').click()
-sleep(2)
 
 # Нажать Checkout
 driver.find_element(By.CSS_SELECTOR, "#checkout").click()
-sleep(1)
 
 # Заполнить форму своими данными
 driver.find_element(By.CSS_SELECTOR, 'input[name="firstName"]').send_keys("Елена")
 driver.find_element(By.CSS_SELECTOR, 'input[name="lastName"]').send_keys("Трегубова")
 driver.find_element(By.CSS_SELECTOR, 'input[name="postalCode"]').send_keys("123456")
-sleep(2)
 
 # Нажать на кнопку "Continue"
 driver.find_element(By.CSS_SELECTOR, 'input[name="continue"]').click()
-sleep(1)
 
 # Прочитать со страницы итоговую стоимость
 txt = WebDriverWait(driver, "10").until(EC.presence_of_element_located((By.CLASS_NAME, "summary_total_label"))).text
