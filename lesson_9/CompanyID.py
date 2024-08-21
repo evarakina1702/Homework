c069a3e49e82241356e55bac73cbde6c1c2144af
evarakina1702 <157830517+evarakina1702@users.noreply.github.com>
2024-08-21 12:56:44 +0400

    Update CompanyID.py

---
+++ lesson_9/CompanyID.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+
+
+class CompanyID:
+    __scripts = {
+        "select": 'select * from company where deleted_at is null',
+        "select_only_active": 'select * from company ' 'where deleted_at is null ' 'and is_active = true',
+        "delete_by_id": text("DELETE FROM company WHERE id = :id_to_delete"),
+        "insert new": text("INSERT INTO company(\"name\", description) " "VALUES (:new_name, :new_description)"),
+        "get_max_id": text("SELECT MAX(id) FROM company"),
+        "select_by_id": text('SELECT * FROM company ' 'WHERE id = :select_id ' 'AND deleted_at is null')
+    }
+
+    def __init__(self, db_connection_string):
+        self.db = create_engine(db_connection_string)
+
+    def create_company(self, name, description):
+        self.db.execute(self.__scripts["insert new"], new_name=name, new_description=description)
+
+    def get_id_company(self):
+        return self.db.execute(self.__scripts["get_max_id"]).fetchall()[0][0]
+    
+    def delete_company(self, id):
+        self.db.execute(self.__scripts["delete_by_id"], id_to_delete=id)
